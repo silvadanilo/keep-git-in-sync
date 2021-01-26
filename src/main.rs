e4b268938fbed14e324490329f96701904892f38
danilo silva <badkill82@gmail.com>
2021-01-26 22:25:51 +0000

    changed inotify with notify

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,125 @@
-extern crate inotify;
+extern crate notify;
 
 use chrono::prelude::*;
-use git2::{Commit, ObjectType, Oid, IndexAddOption, Repository, StatusOptions};
-use inotify::{EventMask, WatchMask, Inotify};
+use git2::{Commit, IndexAddOption, ObjectType, Oid, Repository, StatusOptions};
+use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
 use std::collections::HashMap;
-use std::collections::HashSet;
+use std::path::Path;
 use std::path::PathBuf;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+fn repo(folder: &str) -> Option<Repository> {
+    match Repository::open(folder) {
+        Ok(repo) => Some(repo),
+        Err(_) => None,
+    }
+}
 
 fn main() {
-    let mut v: Vec<PathBuf> = Vec::new();
+    let folders = vec!["/tmp/acp", "/tmp/acp2", "/tmp/acp3"];
 
-    v.push(PathBuf::from("/tmp/acp"));
-    v.push(PathBuf::from("/tmp/acp2"));
+    let repositories: HashMap<_, _> = folders
+        .into_iter()
+        .filter(|folder| Path::new(folder).exists())
+        .filter_map(|folder| match repo(folder) {
+            Some(repo) => Some((folder, repo)),
+            None => None,
+        })
+        .collect();
 
-    let mut inotify = Inotify::init()
-        .expect("Failed to initialize inotify");
+    println!("folders: {:?}", repositories.keys());
 
-    let repositories = v.iter().fold(HashMap::new(), |mut acc, dir| {
-        let wd = inotify
-            .add_watch(
-                dir,
-                WatchMask::MODIFY | WatchMask::CREATE | WatchMask::DELETE,
-            )
-            .expect("Failed to add inotify watch");
-
-        let repo = match Repository::open(dir) {
-            Ok(repo) => repo,
-            Err(e) => panic!("[git] failed to open: {}", e),
-        };
-
-        acc.insert(wd, repo);
-        acc
-    });
-
-    println!("Watching current directory for activity...");
-
-    let mut buffer = [0u8; 4096];
-    loop {
-        let events = inotify
-            .read_events_blocking(&mut buffer)
-            .expect("Failed to read inotify events");
-
-        let mut modified_folders = HashSet::new();
-
-        for event in events {
-            modified_folders.insert(event.wd);
-            if event.mask.contains(EventMask::CREATE) {
-                if event.mask.contains(EventMask::ISDIR) {
-                    println!("Directory created: {:?}", event.name);
-                } else {
-                    println!("File created: {:?}", event.name);
-                }
-            } else if event.mask.contains(EventMask::DELETE) {
-                if event.mask.contains(EventMask::ISDIR) {
-                    println!("Directory deleted: {:?}", event.name);
-                } else {
-                    println!("File deleted: {:?}", event.name);
-                }
-            } else if event.mask.contains(EventMask::MODIFY) {
-                if event.mask.contains(EventMask::ISDIR) {
-                    println!("Directory modified: {:?}", event.name);
-                } else {
-                    println!("File modified: {:?}", event.name);
-                }
-            }
-        }
-
-        println!("end of events for");
-
-        for wd in modified_folders {
-            let repo = repositories.get(&wd).unwrap();
-            println!("is modified {}", is_modified(repo));
-            if is_modified(repo) {
-                add_and_commit(repo, format!("Auto-Commit at: {}", Local::now().format("%Y-%m-%d %H:%M:%S").to_string()));
-            }
-        }
-    }
+    watch(repositories);
 }
 
 fn add_and_commit(repo: &Repository, message: String) -> Result<Oid, git2::Error> {
     let mut index = repo.index()?;
-    index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None).expect("Cannot add file to git index");
+    index
+        .add_all(["*"].iter(), IndexAddOption::DEFAULT, None)
+        .expect("Cannot add file to git index");
     index.write().expect("Cannot write git index");
 
     let oid = index.write_tree()?;
     let signature = repo.signature().expect("Cannot fetch default signature");
     let parent_commit = find_last_commit(&repo)?;
     let tree = repo.find_tree(oid)?;
-    repo.commit(Some("HEAD"), //  point HEAD to our new commit
-                &signature, // author
-                &signature, // committer
-                &message[..], // commit message
-                &tree, // tree
-                &[&parent_commit]) // parents
+    repo.commit(
+        Some("HEAD"), //  point HEAD to our new commit
+        &signature,   // author
+        &signature,   // committer
+        &message[..], // commit message
+        &tree,        // tree
+        &[&parent_commit],
+    ) // parents
 }
 
 fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
     let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
-    obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
+    obj.into_commit()
+        .map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
 fn is_modified(repo: &Repository) -> bool {
     let mut opts = StatusOptions::new();
-    opts
-        .include_untracked(true)
+    opts.include_untracked(true)
         .recurse_untracked_dirs(true)
         .exclude_submodules(true);
 
     let statuses = repo.statuses(Some(&mut opts));
 
     !statuses.unwrap().is_empty()
+}
+
+fn watch(repositories: HashMap<&str, Repository>) -> notify::Result<()> {
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(300))?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    for path in repositories.keys() {
+        watcher.watch(path, RecursiveMode::Recursive)?;
+        watcher.watch(path, RecursiveMode::Recursive)?;
+    }
+
+    // This is a simple loop, but you may want to use more complex logic here,
+    // for example to handle I/O.
+    loop {
+        match rx.recv() {
+            Ok(event) =>
+            {
+                match event {
+                    DebouncedEvent::NoticeWrite(path)
+                    | DebouncedEvent::NoticeRemove(path)
+                    | DebouncedEvent::Create(path)
+                    | DebouncedEvent::Write(path)
+                    | DebouncedEvent::Chmod(path)
+                    | DebouncedEvent::Remove(path)
+                    | DebouncedEvent::Rename(_, path) => {
+                        let repo = related_repository(path, &repositories).unwrap();
+                        if is_modified(repo) {
+                            add_and_commit(repo, format!("Auto-Commit at: {}", Local::now().format("%Y-%m-%d %H:%M:%S").to_string()));
+                        }
+                    },
+                    _ => (),
+                }
+            }
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
+
+fn related_repository<'a>(path: PathBuf, repositories: &'a HashMap<&'a str, Repository>) -> Option<&'a Repository> {
+    let folder = repositories
+        .keys()
+        .find(|folder| path.starts_with(folder));
+
+    match folder {
+        Some(folder) => repositories.get(folder),
+        None => None
+    }
 }
